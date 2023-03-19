8823c1702c5b0361cd9cb93202466be385f505d6
Diego Murillo <diegomurilloferrer@gmail.com>
2023-03-19 11:21:57 -0500

    Develop a fisrt login screen

--- src/pages/register.ts
+++ src/pages/register.ts
@@ -3,12 +3,52 @@ import html from "html-template-tag";
 
 export default class Register extends AbstractComponent {
   render() {
-    const fields: string[] = ["Full Name", "Age"];
-
     return html`
-      <div class="has-text-primary">Register to our amazing app</div>
-      <div>${fields[0]}</div>
-      <div>${fields[1]}</div>
+      <div class="container p-5 mt-3 custom-max-width">
+        <div class="box">
+          <h1 class="has-text-weight-semibold is-size-1">Sign up</h1>
+          <p>
+            Join to our Social Networking app today and start connecting with
+            your global community. Create a free account and set up your
+            personalized profile in just a few easy steps.
+          </p>
+          <form class="mt-3">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input
+                  class="input"
+                  type="email"
+                  placeholder="user@domain.com"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Full Name</label>
+              <div class="control">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Barbara R. Williams"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input class="input" type="password" />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Confirm password</label>
+              <div class="control">
+                <input class="input" type="password" />
+              </div>
+            </div>
+            <button class="button is-primary">Sign up</button>
+          </form>
+        </div>
+      </div>
     `;
   }
 }
