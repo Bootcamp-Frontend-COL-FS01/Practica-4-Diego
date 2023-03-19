0829325175218b2a144b7b64cadd86973fdbb28c
Diego Murillo <diegomurilloferrer@gmail.com>
2023-03-19 10:38:43 -0500

    Test the first home component

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,9 +1,32 @@
 import AbstractComponent from "../shared/abstract-component";
+import html from "html-template-tag";
 
 export default class Home extends AbstractComponent {
   render() {
-    return `
-        <div> Welcome to our home </div>
-        `;
+    return html`
+      <div class="container p-5 mt-3">
+        <div class="box">
+          <h1 class="has-text-weight-semibold is-size-1">Hello there!</h1>
+          <h2 class="has-text-weight-medium is-size-5">
+            Welcome to your favorite Social Networking Site!
+          </h2>
+          <p class="mt-3">
+            Connect with friends, family, and people from all over the world on
+            our platform. Share your thoughts, ideas, and experiences with your
+            community, and discover new perspectives from others. Stay
+            up-to-date with what's happening in your world by following your
+            favorite celebrities, news outlets, and organizations. You can also
+            join groups and communities that share your interests and engage in
+            lively discussions.
+          </p>
+          <div class="mt-3">
+            <a href="/login" class="button is-primary" data-navigo>Log in</a>
+            <a href="/register" class="button is-outlined" data-navigo
+              >Sign up</a
+            >
+          </div>
+        </div>
+      </div>
+    `;
   }
 }
--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,9 +1,8 @@
 import AbstractComponent from "../shared/abstract-component";
+import html from "html-template-tag";
 
 export default class Login extends AbstractComponent {
   render() {
-    return `
-        <div> Login to our beautiful app </div>
-        `;
+    return html` <div>Login to our beautiful app</div> `;
   }
 }
