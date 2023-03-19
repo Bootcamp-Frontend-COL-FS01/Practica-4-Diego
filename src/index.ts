6c22c848c8fc232984e5c561120b9877d753b6e4
Diego Murillo <diegomurilloferrer@gmail.com>
2023-03-18 23:44:30 -0500

    Set basic routing with external lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 import "./index.css";
+import Navigo from "navigo";
+
+const application: HTMLDivElement = document.querySelector("#app")!;
+const routerManager = new Navigo("/", {
+  hash: true,
+});
+
+routerManager.on(() => {
+  application.innerHTML = "<div> I am a div! </div>";
+});
+
+routerManager.on("/login", () => {
+  application.innerHTML = "<div> I am a second div! </div>";
+});
+
+routerManager.resolve();
